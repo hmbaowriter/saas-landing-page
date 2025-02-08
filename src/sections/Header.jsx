b3194adf5ed81bed016338972d43c94e3c339984
Tommy Evans <webdesign.minhbao@gmail.com>
2025-02-08 23:30:56 +0700

    fix transparent header

--- src/sections/Header.jsx
+++ src/sections/Header.jsx
@@ -1,64 +1,92 @@
-import React, { useState } from 'react';
 import { Link as LinkScroll } from 'react-scroll';
+import { useEffect, useState } from 'react';
 import clsx from 'clsx';
 
-const NavLink = ({ title }) => (
-  <LinkScroll className='base-bold text-p4 uppercase transition-colors duration-500 cursor-pointer hover:text-p1 max-lg:my-4 max-lg:h5'>
-    {title}
-  </LinkScroll>
-);
-
 const Header = () => {
-  const [isOpened, setIsOpened] = useState(false);
+  const [hasScrolled, setHasScrolled] = useState(false);
+  const [isOpen, setIsOpen] = useState(false);
+
+  useEffect(() => {
+    const handleScroll = () => {
+      setHasScrolled(window.scrollY > 32);
+    };
+
+    window.addEventListener('scroll', handleScroll);
+
+    return () => {
+      window.removeEventListener('scroll', handleScroll);
+    };
+  }, []);
+
+  const NavLink = ({ title }) => (
+    <LinkScroll
+      onClick={() => setIsOpen(false)}
+      to={title}
+      offset={-100}
+      spy
+      smooth
+      activeClass='nav-active'
+      className='base-bold max-lg:h5 cursor-pointer uppercase text-p4 transition-colors duration-500 hover:text-p1 max-lg:my-4'
+    >
+      {title}
+    </LinkScroll>
+  );
 
   return (
-    <header className='fixed top-0 left-0 z-50 w-full py-10'>
-      <div className='container flex h-14 items-center max-lg:px-50'>
-        <a className='lg:hidden flex-1 cursor-pointer z-2'>
+    <header
+      className={clsx(
+        'fixed left-0 top-0 z-50 w-full py-10 transition-all duration-500 max-lg:py-4',
+        hasScrolled && 'bg-black-100 py-2 backdrop-blur-[8px]',
+      )}
+    >
+      <div className='container flex h-14 items-center max-lg:px-5'>
+        <a className='z-2 flex-1 cursor-pointer lg:hidden'>
           <img src='/images/xora.svg' width={115} height={55} alt='logo' />
         </a>
 
         <div
           className={clsx(
-            'w-full max-lg:fixed max-lg:top-0 max-lg:left-0 max-lg:bg-s2 max-lg:opacity-0',
-            isOpened ? 'max-lg:opacity-100' : 'max-lg:pointer-events-none'
+            'w-full max-lg:fixed max-lg:left-0 max-lg:top-0 max-lg:w-full max-lg:bg-s2 max-lg:opacity-0',
+            isOpen ? 'max-lg:opacity-100' : 'max-lg:pointer-events-none',
           )}
         >
-          <div className='max-lg:relative max-lg:flex max-lg:flex-col max-lg:min-h-screen max-lg:p-6 max-lg:overflow-hidden sidebar-before max-md:px-4'>
+          <div className='sidebar-before max-lg:relative max-lg:flex max-lg:min-h-screen max-lg:flex-col max-lg:overflow-hidden max-lg:p-6 max-md:px-4'>
             <nav className='max-lg:relative max-lg:z-2 max-lg:my-auto'>
               <ul className='flex max-lg:block max-lg:px-12'>
                 <li className='nav-li'>
-                  <NavLink title={'features'} />
+                  <NavLink title='features' />
                   <div className='dot' />
-                  <NavLink title={'pricing'} />
+                  <NavLink title='pricing' />
                 </li>
+
                 <li className='nav-logo'>
                   <LinkScroll
                     to='hero'
-                    offset={-100}
-                    spy={true}
-                    smooth={true}
+                    offset={-250}
+                    spy
+                    smooth
                     className={clsx(
-                      'max-lg:hidden transition-transform duration-500  cursor-pointer'
+                      'cursor-pointer transition-transform duration-500 max-lg:hidden',
                     )}
                   >
                     <img
                       src='/images/xora.svg'
                       width={160}
                       height={55}
-                      alt=''
+                      alt='logo'
                     />
                   </LinkScroll>
                 </li>
+
                 <li className='nav-li'>
-                  <NavLink title={'faq'} />
+                  <NavLink title='faq' />
                   <div className='dot' />
-                  <NavLink title={'download'} />
+                  <NavLink title='download' />
                 </li>
               </ul>
             </nav>
 
-            <div className='lg:hidden block absolute top-1/2 left-0 w-[960px] h-[380px] translate-x-[-290px] -translate-y-1/2 rotate-90'>
+            <div className='absolute left-0 top-1/2 block h-[380px] w-[960px] -translate-y-1/2 translate-x-[-290px] rotate-90 lg:hidden'>
               <img
                 src='/images/bg-outlines.svg'
                 width={960}
@@ -71,20 +99,20 @@ const Header = () => {
                 width={960}
                 height={380}
                 alt='outline'
-                className='absolute inset-0 mix-blend-soft-light opacity-5'
+                className='absolute inset-0 opacity-5 mix-blend-soft-light'
               />
             </div>
           </div>
         </div>
 
         <button
-          className='lg:hidden z-2 size-10 border-2 border-s4/25 rounded-full flex justify-center items-center'
-          onClick={() => setIsOpened((prev) => !prev)}
+          className='z-2 flex size-10 items-center justify-center rounded-full border-2 border-s4/25 lg:hidden'
+          onClick={() => setIsOpen((prevState) => !prevState)}
         >
           <img
-            src={`/images/${isOpened ? 'close' : 'magic'}.svg`}
+            src={`/images/${isOpen ? 'close' : 'magic'}.svg`}
+            alt='magic'
             className='size-1/2 object-contain'
-            alt=''
           />
         </button>
       </div>
