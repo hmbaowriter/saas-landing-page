7cabf759629150fbfd953723973e2e3bbb76b73f
Tommy Evans <webdesign.minhbao@gmail.com>
2025-02-08 18:32:55 +0700

    add header

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,11 @@
 import React from 'react'
+import Header from './sections/Header'
 
 const App = () => {
   return (
-    <div>App</div>
+    <main className='overflow-hidden'>
+      <Header />
+    </main>
   )
 }
 
