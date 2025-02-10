89c599764776232f6ac8e2932983b41b0c85a712
Tommy Evans <webdesign.minhbao@gmail.com>
2025-02-10 15:32:49 +0700

    complete footer section

--- src/App.jsx
+++ src/App.jsx
@@ -7,6 +7,7 @@ import Pricing from './sections/Pricing';
 import Faq from './sections/Faq';
 import Testimonials from './sections/Testimonials';
 import Download from './sections/Download';
+import Footer from './sections/Footer';
 
 const App = () => {
   return (
@@ -18,6 +19,7 @@ const App = () => {
       <Faq />
       <Testimonials />
       <Download />
+      <Footer />
     </main>
   );
 };
