e4958ddbe24bfff5cd6daa67a8f9d4eb9be23819
Tommy Evans <webdesign.minhbao@gmail.com>
2025-02-10 15:27:55 +0700

    complete download section

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,12 @@
 import React from 'react';
+
 import Header from './sections/Header';
 import Hero from './sections/Hero';
 import Features from './sections/Features';
 import Pricing from './sections/Pricing';
 import Faq from './sections/Faq';
 import Testimonials from './sections/Testimonials';
+import Download from './sections/Download';
 
 const App = () => {
   return (
@@ -15,6 +17,7 @@ const App = () => {
       <Pricing />
       <Faq />
       <Testimonials />
+      <Download />
     </main>
   );
 };
